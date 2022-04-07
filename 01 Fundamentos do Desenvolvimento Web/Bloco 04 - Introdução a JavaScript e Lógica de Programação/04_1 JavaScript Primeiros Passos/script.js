bad33a6fad7201d50afd7ab65fed162c4459cc09
Raynara Santiago <ray@MacBook-Air-de-Raynara.local>
2022-04-06 22:59:01 -0300

    adicioona programa que retorna o maior valor de três

--- 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_1 JavaScript Primeiros Passos/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_1 JavaScript Primeiros Passos/script.js
@@ -1,16 +1,27 @@
-// const a = 6;
-// const b = 4;
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-// console.log(a % b);
+//const a = 6;
+//const b = 4;
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+//console.log(a % b);
 
-const dois = 105;
-const um = 140;
+//const dois = 105;
+//const um = 140;
+//if (um > dois) {
+// console.log(um);
+//} else {
+// console.log(dois);
+//}
 
-if (um > dois) {
-  console.log(um);
+const primeira = 12;
+const segunda = 15;
+const terceira = 9;
+
+if (primeira > segunda && terceira) {
+  console.log(primeira);
+} else if (segunda > primeira && terceira) {
+  console.log(segunda);
 } else {
-  console.log(dois);
+  console.log(terceira);
 }
