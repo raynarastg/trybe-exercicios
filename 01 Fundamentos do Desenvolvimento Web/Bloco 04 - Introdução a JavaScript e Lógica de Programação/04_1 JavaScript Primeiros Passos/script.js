b9e6b8c1a3236dc04cff4a0c6044a157e4fc988e
Raynara Santiago <ray@MacBook-Air-de-Raynara.local>
2022-04-06 22:39:31 -0300

    adiciona programa que retornar o maior valor

--- 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_1 JavaScript Primeiros Passos/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_1 JavaScript Primeiros Passos/script.js
@@ -1,7 +1,16 @@
-const a = 6;
-const b = 4;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// const a = 6;
+// const b = 4;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+const dois = 105;
+const um = 140;
+
+if (um > dois) {
+  console.log(um);
+} else {
+  console.log(dois);
+}
