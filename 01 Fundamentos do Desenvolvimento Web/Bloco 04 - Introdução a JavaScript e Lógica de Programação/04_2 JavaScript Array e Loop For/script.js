ffc19a7c9ed112a68dcf6a46e2ee9c8d194ce30a
Raynara Santiago <ray@MacBook-Air-de-Raynara.local>
2022-04-07 20:32:12 -0300

    soma todos valores e imprime resultado

--- 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_2 JavaScript Array e Loop For/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_2 JavaScript Array e Loop For/script.js
@@ -1,4 +1,11 @@
+//exercicio 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index++) {
   console.log(numbers[index]);
 }
+// ex 2
+var soma = 0;
+for (let index = 0; index < numbers.length; index++) {
+  soma = soma + numbers[index];
+}
+console.log(soma);
