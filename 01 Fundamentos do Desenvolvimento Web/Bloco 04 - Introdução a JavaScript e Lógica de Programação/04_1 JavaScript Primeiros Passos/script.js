3c8ef8f1ee70ea36c0a8a8531499dd5a4a9e689d
Raynara Santiago <ray@MacBook-Air-de-Raynara.local>
2022-04-06 23:46:56 -0300

    adiciona progama que retorna se é negativo, positivo ou zero

--- 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_1 JavaScript Primeiros Passos/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_1 JavaScript Primeiros Passos/script.js
@@ -1,27 +1,36 @@
-//const a = 6;
-//const b = 4;
-//console.log(a + b);
-//console.log(a - b);
-//console.log(a * b);
-//console.log(a / b);
-//console.log(a % b);
+const a = 6;
+const b = 4;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
-//const dois = 105;
-//const um = 140;
-//if (um > dois) {
-// console.log(um);
-//} else {
-// console.log(dois);
-//}
+const dois = 105;
+const um = 140;
+if (um > dois) {
+  console.log(um);
+} else {
+  console.log(dois);
+}
 
 const primeira = 12;
 const segunda = 15;
 const terceira = 9;
-
 if (primeira > segunda && terceira) {
   console.log(primeira);
 } else if (segunda > primeira && terceira) {
   console.log(segunda);
 } else {
   console.log(terceira);
+}
+
+const valor = 107;
+
+if (valor > 0) {
+  console.log("positive");
+} else if (valor < 0) {
+  console.log("negative");
+} else {
+  console.log("zero");
 }
