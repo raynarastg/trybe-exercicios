261a51d484f2063ec0bf8804203524bc2a62cf2a
Raynara Santiago <raysantiagobz@gmail.com>
2022-04-08 21:19:36 -0300

    pega a maior e a menor palavra dentro do array

--- 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_3 JavaScript Lógica de Programação e Algoritmos/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 04 - Introdução a JavaScript e Lógica de Programação/04_3 JavaScript Lógica de Programação e Algoritmos/script.js
@@ -10,3 +10,22 @@ let word = "tryber";
 let reverso = "";
 reverso = word.split("").reverse().join("");
 console.log(reverso);
+//exercicio 3
+let array = ["java", "javascript", "python", "html", "css"];
+
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for (let index = 0; index < array.length; index++) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
+}
+console.log(maiorPalavra);
+
+for (let index = 0; index < array.length; index++) {
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+}
+console.log(menorPalavra);
