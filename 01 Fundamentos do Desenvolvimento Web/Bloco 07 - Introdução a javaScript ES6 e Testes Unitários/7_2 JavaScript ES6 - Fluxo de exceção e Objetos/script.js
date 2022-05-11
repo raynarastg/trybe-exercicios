6bf8775f3298d4e2575bf8561615d94ce96fd745
Raynara Santiago <raysantiagobz@gmail.com>
2022-05-11 11:50:52 -0300

    adiciona finally

--- 01 Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução a javaScript ES6 e Testes Unitários/7_2 JavaScript ES6 - Fluxo de exceção e Objetos/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução a javaScript ES6 e Testes Unitários/7_2 JavaScript ES6 - Fluxo de exceção e Objetos/script.js
@@ -18,17 +18,21 @@ const soma = () => {
   const value2 = document.getElementById("value2").value;
   const result = parseInt(value1) + parseInt(value2);
   const resultado = document.getElementById("result");
-  document.getElementById("value1").value = " ";
-  document.getElementById("value2").value = " ";
+  
   try {
     verificaValue(value1, value2);
     resultado.innerHTML = `Resultado: ${result}`;
   } catch (error) {
     resultado.innerHTML = `Erro: ${error.message}`;
   }
+  finally{
+    document.getElementById("value1").value = " ";
+      document.getElementById("value2").value = " ";
+    }
 };
 
 window.onload = () => {
   const button = document.getElementById("button");
   button.addEventListener("click", soma);
 };
+//
