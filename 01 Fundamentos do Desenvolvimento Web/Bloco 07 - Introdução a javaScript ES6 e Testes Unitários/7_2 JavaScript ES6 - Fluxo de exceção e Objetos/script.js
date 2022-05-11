d63918b133d1a89c8f89def773039823a4a53b10
Raynara Santiago <raysantiagobz@gmail.com>
2022-05-11 12:02:38 -0300

    adiciona exercicio parte 2

--- 01 Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução a javaScript ES6 e Testes Unitários/7_2 JavaScript ES6 - Fluxo de exceção e Objetos/script.js
+++ 01 Fundamentos do Desenvolvimento Web/Bloco 07 - Introdução a javaScript ES6 e Testes Unitários/7_2 JavaScript ES6 - Fluxo de exceção e Objetos/script.js
@@ -5,34 +5,89 @@
 //   document.getElementById("result").innerHTML = `Resultado: ${result}`;
 //   document.getElementById("value1").value = "";
 //   document.getElementById("value2").value = "";
-//
-const verificaValue = (value1, value2) => {
-  if ((value1 === " " || value2 === " ") || (isNaN(value1) || isNaN(value2))) {
-    throw new Error(
-      "São necessários dois valores para que a operação seja feita! Os valores devem ser númericos"
-    );
-  }
-};
-const soma = () => {
-  const value1 = document.getElementById("value1").value;
-  const value2 = document.getElementById("value2").value;
-  const result = parseInt(value1) + parseInt(value2);
-  const resultado = document.getElementById("result");
+//////
+// const verificaValue = (value1, value2) => {
+//   if ((value1 === " " || value2 === " ") || (isNaN(value1) || isNaN(value2))) {
+//     throw new Error(
+//       "São necessários dois valores para que a operação seja feita! Os valores devem ser númericos"
+//     );
+//   }
+// };
+// const soma = () => {
+//   const value1 = document.getElementById("value1").value;
+//   const value2 = document.getElementById("value2").value;
+//   const result = parseInt(value1) + parseInt(value2);
+//   const resultado = document.getElementById("result");
   
-  try {
-    verificaValue(value1, value2);
-    resultado.innerHTML = `Resultado: ${result}`;
-  } catch (error) {
-    resultado.innerHTML = `Erro: ${error.message}`;
-  }
-  finally{
-    document.getElementById("value1").value = " ";
-      document.getElementById("value2").value = " ";
+//   try {
+//     verificaValue(value1, value2);
+//     resultado.innerHTML = `Resultado: ${result}`;
+//   } catch (error) {
+//     resultado.innerHTML = `Erro: ${error.message}`;
+//   }
+//   finally{
+//     document.getElementById("value1").value = " ";
+//       document.getElementById("value2").value = " ";
+//     }
+// };
+
+// window.onload = () => {
+//   const button = document.getElementById("button");
+//   button.addEventListener("click", soma);
+// };
+/// parte 2
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
     }
+  },
+  payment: {
+    total: 60,
+  },
 };
 
-window.onload = () => {
-  const button = document.getElementById("button");
-  button.addEventListener("click", soma);
-};
-//
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  
+    return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP ${order.address.apartment}`
+   
+   
+
+}
+
+console.log(customerInfo(order));
+
+// const orderModifier = (order) => {
+//   // Adicione abaixo as informações necessárias.
+
+// }
+
+// orderModifier(order);
+
+
