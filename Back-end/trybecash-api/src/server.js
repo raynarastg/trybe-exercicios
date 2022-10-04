842163dfd202185582fa1f0d37990897480a844d
Raynara Santiago <raysantiagobz@gmail.com>
2022-10-04 00:01:47 -0300

    fixando

--- Back-end/trybecash-api/src/server.js
+++ Back-end/trybecash-api/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('./app');
 const connection = require('./db/connection');
 
@@ -5,7 +6,7 @@ const port = 3001;
 
 app.listen(port, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${port}`);
-
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
   // O código abaixo é para testarmos a comunicação com o MySQL
   const [ result ] = await connection.execute('SELECT 1');
   if (result) {
