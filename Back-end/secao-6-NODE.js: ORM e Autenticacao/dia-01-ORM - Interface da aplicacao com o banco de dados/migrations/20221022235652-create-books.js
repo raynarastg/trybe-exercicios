2c80de625e5df3aa811cc00d875df065bf1b18d5
Raynara Santiago <raysantiagobz@gmail.com>
2022-10-23 16:38:19 -0300

    seção 6 dia 1

---
+++ Back-end/secao-6-NODE.js: ORM e Autenticacao/dia-01-ORM - Interface da aplicacao com o banco de dados/migrations/20221022235652-create-books.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+ up : (queryInterface) => {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      }
+    });
+  },
+ down : (queryInterface) => {
+   return queryInterface.dropTable('Books');
+  },
+};
